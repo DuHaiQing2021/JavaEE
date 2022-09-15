d08fc296c4f1ca8bd79e8a92f2e9782a1b69fe7c
DuHaiQing2021 <792482948@qq.com>
2022-09-15 23:46:00 +0800

    2022.9.15

---
+++ Test22_9_15/src/main/java/GetParameterServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/getParamerter")
+public class GetParameterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        String userId=req.getParameter("userId");
+        String classId=req.getParameter("classId");
+
+        resp.getWriter().write("userId="+userId+", classId="+classId);
+
+    }
+}
---
+++ Test22_9_15/src/main/java/PostGetparameterServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/postGetParameter")
+public class PostGetparameterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        String userId=req.getParameter("userId");
+        String classId=req.getParameter("classId");
+
+        resp.getWriter().write("userId="+userId+",classId="+classId);
+
+    }
+
+
+}
